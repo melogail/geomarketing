5456fed7cf42bf1d638783f05b86ff4f88c2627a
PC <PC@DESKTOP-HOPA585.Academy.local>
2020-12-28 11:52:44 +0200

    ignore

--- classes/Scrap.py
+++ classes/Scrap.py
@@ -29,6 +29,7 @@ class Scrap(object):
             driver_path = web_driver_config['chrome']['path']
             driver_options = webdriver.ChromeOptions()
             driver_options.add_argument("--lang=en_UK")
+            driver_options.add_argument('disable-infobars')
             return webdriver.Chrome(driver_path, chrome_options=driver_options)
 
     def run(self, query):
